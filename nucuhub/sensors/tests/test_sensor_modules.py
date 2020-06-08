1c48ff4ff060e7b70e49d577a5844854745b532c
Denis Nutiu <denis.nutiu@gmail.com>
2020-06-08 13:43:43 +0300

    NUH-2: Fix test_cpu_temperature_read test

--- nucuhub/sensors/tests/test_sensor_modules.py
+++ nucuhub/sensors/tests/test_sensor_modules.py
@@ -1,8 +1,15 @@
+import tempfile
+
 from nucuhub.sensors.modules import CpuTemperature
 
 
 def test_cpu_temperature_read(redis_fixture):
-    sensor = CpuTemperature()
-    sensor.enable()
-    data = sensor.get_data()
-    assert data is not None
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        f.writelines("13000")
+        f.flush()
+
+        sensor = CpuTemperature()
+        sensor.file_name = f.name
+        sensor.enable()
+        data = sensor.get_data()
+        assert data is not None
