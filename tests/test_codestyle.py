09a7b8523d177c029c0461535a557b5359f04f1c
Denis Nutiu <denis.nutiu@gmail.com>
2020-07-11 12:11:27 +0300

    skip venv when using black and isort

--- tests/test_codestyle.py
+++ tests/test_codestyle.py
@@ -2,12 +2,16 @@ import subprocess
 
 
 def test_black():
-    result = subprocess.run(["black", "--check", "--diff", "."], timeout=5,)
+    result = subprocess.run(
+        ["black", "--check", "--diff", "--exclude=venv", "."], timeout=5,
+    )
 
     assert result.returncode == 0, result.stdout
 
 
 def test_isort():
-    result = subprocess.run(["isort", "--check-only", "--diff"], timeout=5,)
+    result = subprocess.run(
+        ["isort", "--check-only", "--diff", "--skip=venv"], timeout=5,
+    )
 
     assert result.returncode == 0, result.stdout
