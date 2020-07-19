c62ff3b68023fa9534055217176baac460c9707b
Denis Nutiu <denis.nutiu@gmail.com>
2020-07-19 15:41:31 +0300

    Add environment variables in docker-compose.prod.yml

--- nucuhub/config.py
+++ nucuhub/config.py
@@ -2,9 +2,13 @@ import os
 
 
 class ApplicationConfig:
+    # If true the backend will run in debug mode.
     BACKEND_DEBUG = bool(os.getenv("BACKEND_DEBUG", False))
+    # Affects the logging level across all modules.
     LOGGING_LEVEL = os.getenv("LOG_LEVEL")
+    # Override the REDIS_URL, useful for testing & debugging.
     REDIS_URL = os.getenv("REDIS_URL") or "redis_service"
+    # Firebase related config.
     FIREBASE_API_KEY = os.getenv("FIREBASE_API_KEY")
     FIREBASE_AUTH_DOMAIN = os.getenv("FIREBASE_AUTH_DOMAIN")
     FIREBASE_DATABASE_URL = os.getenv("FIREBASE_DATABASE_URL")
